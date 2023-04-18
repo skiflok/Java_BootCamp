ab8ce98e7d93194dfec67629a6578230c8845c05
violator <violator@student.21-school.ru>
2023-04-18 16:36:53 +0300

    04

--- day00/src/ex04/Program.java
+++ day00/src/ex04/Program.java
@@ -1,10 +1,19 @@
 package ex04;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
+
+        String testString = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASSSSSSSSSSSSSSS" +
+                "SSSSSSSSSDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDWEWWKFK" +
+                "KDKKDSKAKLSLDKSKALLLLLLLLLLRTRTETWTWWWWWWW" +
+                "WWWOOOOOOO42";
+        InputStream is = new ByteArrayInputStream(testString.getBytes());
+        System.setIn(is);
 
         Scanner scanner = new Scanner(System.in);
 
@@ -21,72 +30,76 @@ public class Program {
         char[] dataArray = data.toCharArray();
         int[] frequencies = new int[65536];
 
+        int charSize = 0;
+
         for (char value : dataArray) {
             c = value;
+            if (frequencies[c] == 0) {
+                charSize++;
+            }
             frequencies[c]++;
         }
 
-        int [][] res = new int[65536][2];
+        int[][] res = new int[charSize][2];
         int charCount = 0;
         for (int i = 0; i < frequencies.length; i++) {
             if (frequencies[i] != 0) {
                 res[charCount][0] = i;
-                res[charCount][1] = frequencies [i];
+                res[charCount][1] = frequencies[i];
                 charCount++;
             }
         }
 
         for (int i = 0; i < charCount - 1; i++) {
             for (int j = 0; j < charCount - i - 1; j++) {
-                if (res[j][1] < res[j+1][1] || (res[j][1] == res[j+1][1] && res[j][0] > res[j+1][0])) {
+                if (res[j][1] < res[j + 1][1] || (res[j][1] == res[j + 1][1] && res[j][0] > res[j + 1][0])) {
                     int[] temp = res[j];
-                    res[j] = res[j+1];
-                    res[j+1] = temp;
+                    res[j] = res[j + 1];
+                    res[j + 1] = temp;
                 }
             }
         }
 
 
-
-
-
-
-        for (int i = 0; i < charCount; i++) {
-            System.out.printf("%c: %d\n", res[i][0], res[i][1]);
-        }
-
-
-        int height = 10;
-        int max = res[0][1];
-//        for (int i = 0; i < charCount; i++) {
-//            for (int j = 0; j < height; j++) {
-//                if (res[charCount][1] == max) {
-//                    System.out.printf("%d", res[i][1]);
-//                } else {
-//                    System.out.printf("#");
-//                }
-//                System.out.println();
-//            }
-//        }
-
-        for (int i = 1; i <= height; i++) {
-            for (int j = 0; j < charCount; j++) {
-                if (res[j][1] == max / i) {
-                    System.out.printf("%d", res[i][1]);
-                } else  {
-                    System.out.print("#");
+        double max = res[0][1];
+        double range = max/10;
+        for (double y = max + range; y > 0; y-= range) {
+            for (int x = 0; x < charCount && x < 10; x++) {
+                if (res[x][1] >= y - range && res[x][1] < y) {
+                    System.out.printf("%3d ", res[x][1]);
+                } else if (res[x][1] >= y - range) {
+                        System.out.printf("%3s ", "#");
                 }
             }
-            System.out.println();
+            if (y != range) {
+                System.out.println();
+            }
         }
-
-        System.out.println();
-        for (int i = 0; i < charCount; i++) {
-            System.out.printf("%c", res[i][0]);
+        for (int x = 0; x < charCount && x < 10; x++) {
+            System.out.printf("%3c ", res[x][0]);
         }
 
     }
 
+//    public static void printRes() {
+//        double max = res[0][1];
+//        double range = max/10;
+//        for (double y = max + range; y > 0; y-= range) {
+//            for (int x = 0; x < charCount && x < 10; x++) {
+//                if (res[x][1] >= y - range && res[x][1] < y) {
+//                    System.out.printf("%3d ", res[x][1]);
+//                } else if (res[x][1] >= y - range) {
+//                    System.out.printf("%3s ", "#");
+//                }
+//            }
+//            if (y != range) {
+//                System.out.println();
+//            }
+//        }
+//        for (int x = 0; x < charCount && x < 10; x++) {
+//            System.out.printf("%3c ", res[x][0]);
+//        }
+//    }
     public static void exitApp(Scanner scanner) {
         scanner.close();
         System.err.print("Illegal Argument");
