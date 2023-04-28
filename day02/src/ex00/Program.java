75a7e3650db3e6513327c2b904a88b0fca00a831
violator <violator@student.21-school.ru>
2023-04-28 12:41:00 +0300

    00

--- day02/src/ex00/Program.java
+++ day02/src/ex00/Program.java
@@ -4,6 +4,16 @@ public class Program {
 
     public static void main(String[] args) {
 
+
+        //TODO заполнить файл сигнатурами
+        //TODO открыть файл
+        //TODO определить сигнатуру
+        //TODO вывести статус команду
+        //TODO записать в файл фип файла
+
+
+
+
     }
 
 }
