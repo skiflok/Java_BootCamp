f84e2d851dfb13628d5981670f6428dd1d31b062
violator <violator@student.21-school.ru>
2023-05-03 17:13:28 +0300

    02

--- day02/src/ex02/FileManager.java
+++ day02/src/ex02/FileManager.java
@@ -1,25 +1,53 @@
 package ex02;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileManager {
-
     private Path currentDirectory;
 
     public FileManager(String path) {
         this.currentDirectory = Paths.get(path);
     }
-
     public Path getCurrentDirectory() {
         return currentDirectory;
     }
+    public void mv() {
 
-    public void mv () {}
+    }
+    public void ls(Path path) {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+            for (Path file : stream) {
+                if (Files.isDirectory(file)) {
+                    System.out.printf("%s %d KB\n", file.getFileName(), getDirectorySize(file) / 1024);
+                } else {
+                    System.out.printf("%s %d KB\n", file.getFileName(), Files.size(file) / 1024);
+                }
+            }
+        } catch (IOException e) {
+            System.err.printf("Ошибка обращения к дериктории %s \n",e.getMessage());
+        }
+    }
+    public void cd() {
 
-    public void ls (Path path) {}
+    }
 
-    public void cd () {}
+    public long getDirectorySize(Path path) {
+        long totalSize = 0;
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+            for (Path file : stream) {
+                if (Files.isDirectory(file)) {
+                    totalSize += getDirectorySize(file);
+                } else {
+                    totalSize += Files.size(file);
+                }
+            }
+        } catch (IOException ignore) {
+        }
+        return totalSize;
+    }
 
 }
--- day02/src/ex02/Program.java
+++ day02/src/ex02/Program.java
@@ -1,5 +1,17 @@
 package ex02;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.AccessDeniedException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Program {
 
     public static void main(String[] args) {
@@ -9,13 +21,46 @@ public class Program {
             return;
         }
 
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+        String input;
+        String [] inputToArray;
+        while (true) {
+            try {
+                if ((input = bis.readLine()).equals("exit")) break;
+                inputToArray = input.split(" ");
+                switch (inputToArray[0]) {
+                    case "mv" :
+                        break;
+                    case "ls" :
+                        break;
+                    case "cd" :
+                        break;
+                    default:
+                        System.err.println("Неверная команда");
+                }
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
         String path = args[0];
+        Path directoryPath = Paths.get("/Users/violator/");
+//        Path directoryPath = Paths.get(path);
 
         FileManager fileManager = new FileManager(path);
 
-        System.out.println(fileManager.getCurrentDirectory());
+        fileManager.ls(directoryPath);
 
+
+        try {
+            bis.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-
-
 }
+
