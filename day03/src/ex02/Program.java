8fda2375222b0e7efc7c2c2f3ef513ef9b57fedd
violator <violator@student.21-school.ru>
2023-05-07 15:28:13 +0300

    02 CommandLineArguments.java

--- day03/src/ex02/Program.java
+++ day03/src/ex02/Program.java
@@ -11,11 +11,14 @@ public class Program {
     public static void main(String[] args) {
 
         try {
-            final HashMap<String, Integer> inputParamsToUse = checkAndGetParam(args);
-            int arraySize = inputParamsToUse.get("--arraySize");
-            int threadCount = inputParamsToUse.get("--threadsCount");
+            CommandLineArguments.fiiArguments(args);
+            int arraySize = CommandLineArguments.getParamValue("--arraySize");
+            int threadCount = CommandLineArguments.getParamValue("--threadsCount");
 
-            System.out.println(inputParamsToUse);
+            System.out.println(arraySize);
+            System.out.println(threadCount);
+
+
 
 //            run(count);
         } catch (NumberFormatException e) {
@@ -27,38 +30,5 @@ public class Program {
 
     private static void run(int count) {
         final Object monitor = new Object();
-
-    }
-
-    public static HashMap<String, Integer> checkAndGetParam(String[] args) {
-
-        final List<String> validParam = new LinkedList<>(
-                Arrays.asList("--arraySize", "--threadsCount")
-        );
-
-        final HashMap<String, Integer> inputParamsToUse = new HashMap<>();
-
-        if (args.length != 2) {
-            throw new IllegalArgumentException("Неверное количество аргументов");
-        }
-
-        String[] param;
-        int numericParameter;
-        for (String s : args) {
-            param = s.split("=");
-            if (param.length != 2 || !validParam.contains(param[0])) {
-                throw new IllegalArgumentException("Неизвестнные параметры ввода");
-            }
-            if (inputParamsToUse.containsKey(param[0])) {
-                throw new IllegalArgumentException("Параметры ввода дублируются");
-            }
-            numericParameter = Integer.parseInt(param[1]);
-            if (numericParameter <= 0) {
-                throw new IllegalArgumentException("Некорректный ввод числового параметра");
-            }
-            inputParamsToUse.put(param[0], numericParameter);
-        }
-
-        return inputParamsToUse;
     }
 }
