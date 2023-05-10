9ed550e20d0801bb29de9932b29ab880fc187852
violator <violator@student.21-school.ru>
2023-05-10 16:38:27 +0300

    03 simple download

--- day03/src/ex03/Program.java
+++ day03/src/ex03/Program.java
@@ -1,9 +1,8 @@
 package ex03;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
@@ -18,22 +17,37 @@ public class Program {
 
             String file = "./files_url.txt";
             Path filePath = Paths.get(file);
-            System.out.println(filePath);
 
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            HashMap<String, String> urls = new HashMap<>();
-            String [] temp;
-            while (reader.ready()) {
-                temp = reader.readLine().split(" ");
-                if (temp.length != 2) throw new IllegalArgumentException("Bad args");
-                urls.put(temp[0], temp[1]);
-            }
+            HashMap<String, String> urls = readFileWithResourceToDownload(file);
 
             for (Map.Entry<String, String> entry: urls.entrySet()) {
-                System.out.println(entry);
+//                System.out.println(entry);
+            }
+
+            String downloadDirectory = "./download/";
+            Path downloadDirectoryPath = Paths.get(downloadDirectory);
+
+            if (Files.exists(downloadDirectoryPath)) {
+                System.out.println("exist");
+            } else {
+                System.out.println("not exist");
+                System.out.println("directory create");
+                Files.createDirectory(downloadDirectoryPath);
             }
 
 
+
+            BufferedOutputStream fileWriter = new BufferedOutputStream(
+                    new FileOutputStream(downloadDirectory + "11192eba63f6f3aa591d3263fdb66bd5.jpg"));
+
+            BufferedInputStream urlReader = new BufferedInputStream(
+                    new URL("https://i.pinimg.com/originals/11/19/2e/11192eba63f6f3aa591d3263fdb66bd5.jpg").openStream());
+
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = urlReader.read(dataBuffer, 0, 1024)) != -1) {
+                fileWriter.write(dataBuffer, 0, bytesRead);
+            }
 
             run(threadsCount);
 
@@ -48,25 +62,22 @@ public class Program {
     }
 
     private static void run (int threadsCount) {
-        System.out.println("threadsCount = " + threadsCount);
+//        System.out.println("threadsCount = " + threadsCount);
     }
 
-//    private static HashMap<String, String> readFile(String path) {
-//        HashMap<String, String> urls = new HashMap<>();
-//        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
-//            String [] temp;
-//            while (reader.ready()) {
-//                temp = reader.readLine().split(" ");
-//                if (temp.length != 2) throw new IllegalArgumentException("Bad args");
-//                urls.put(temp[0], temp[1]);
-//            }
-//            for (Map.Entry<String, String> entry: urls.entrySet()) {
-//                System.out.println(entry);
-//            }
-//        } catch (IOException exception) {
-//            exception.printStackTrace();
-//        }
-//        return urls;
-//    }
+    private static HashMap<String, String> readFileWithResourceToDownload(String path) {
+        HashMap<String, String> urls = new LinkedHashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String [] temp;
+            while (reader.ready()) {
+                temp = reader.readLine().split(" ");
+                if (temp.length != 2) throw new IllegalArgumentException("Bad args");
+                urls.put(temp[0], temp[1]);
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return urls;
+    }
 
 }
--- day03/src/ex03/script.sh
+++ day03/src/ex03/script.sh
@@ -3,6 +3,7 @@ if [ $# -ne 1 ]; then
   echo "Usage: $0 arg1"
   exit 1
 fi
+rm -rf download
 # shellcheck disable=SC2035
 javac -d bin *.java
 java -classpath bin ex03.Program "$1"
