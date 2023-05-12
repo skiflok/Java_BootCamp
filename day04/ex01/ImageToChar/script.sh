dcd6470120946aa26caf875873899a98449fe233
skiflok <anisimov.a.v@inbox.ru>
2023-05-12 13:00:03 +0300

    01 manifest script

--- day04/ex01/ImageToChar/script.sh
+++ day04/ex01/ImageToChar/script.sh
@@ -13,3 +13,9 @@ fi
 javac -d target src/java/edu/school21/printer/app/*.java src/java/edu/school21/printer/logic/*.java
 
 java -classpath target edu.school21.printer.app.Main "$@"
+
+cp -r src/resources target
+
+jar -cfm target/images-to-chars-printer.jar src/manifest.txt -C target .
+
+java -jar target/images-to-chars-printer.jar . 0
