9d624e1affc955cc5e9f740b7c4d555ebf320a49
violator <violator@student.21-school.ru>
2023-05-12 19:43:44 +0300

    02 script error

--- day04/ex02/ImageToChar/script.sh
+++ day04/ex02/ImageToChar/script.sh
@@ -9,11 +9,11 @@ else
     mkdir "target"
 fi
 
-# shellcheck disable=SC2046
-javac -d target -cp src/java $(find src/java -name '*.java')
 
 cp -r src/resources target
+# shellcheck disable=SC2046
+javac -d target -cp lib/jcommander-1.82.jar:lib/JCDP-4.0.2.jar $(find src/java -name '*.java')
 
 jar -cfm target/images-to-chars-printer.jar src/manifest.txt -C target .
 
-java -jar target/images-to-chars-printer.jar "$@"
+java -jar target/images-to-chars-printer.jar --white=RED --black=GREEN
