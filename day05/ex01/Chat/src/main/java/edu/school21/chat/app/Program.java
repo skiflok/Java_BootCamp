82d2706a46ef112706fa94216a65d81668ddf519
violator <violator@student.21-school.ru>
2023-05-17 17:53:31 +0300

    01 try JDBC

--- day05/ex01/Chat/src/main/java/edu/school21/chat/app/Program.java
+++ day05/ex01/Chat/src/main/java/edu/school21/chat/app/Program.java
@@ -9,7 +9,7 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("Hello Chat!");
 
-        try (Connection connection  = DriverManager.getConnection(
+        try (Connection connection = DriverManager.getConnection(
                 "jdbc:postgresql://localhost:5432/chatDataBase",
                 "postgres",
                 "admin"
@@ -21,15 +21,17 @@ public class Program {
 
                 int id = results.getInt(1);
                 String name = results.getString(2);
-                System.out.println(results.getRow() + ". " + id + "\t"+ name);
+                System.out.println(results.getRow() + ". " + id + "\t" + name);
             }
+
+            int rowsCount = statement.executeUpdate("UPDATE chat.user set password='newPass' where id=1 ");
+            System.out.println("rowsCount = " +  rowsCount);
+//            rowsCount = statement.executeUpdate("delete from chat.user where id=1 ");
+            System.out.println("rowsCount = " +  rowsCount);
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
-
-
 
     }
 }
