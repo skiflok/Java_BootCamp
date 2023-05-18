d5aaa19f8df000ee75cc42374cad72c00c4794dd
violator <violator@student.21-school.ru>
2023-05-18 19:30:24 +0300

    01 it's a bad day... simple request

--- day05/ex01/Chat/src/main/java/edu/school21/chat/app/Program.java
+++ day05/ex01/Chat/src/main/java/edu/school21/chat/app/Program.java
@@ -1,5 +1,8 @@
 package edu.school21.chat.app;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.*;
 
 /**
@@ -12,24 +15,36 @@ public class Program {
         try (Connection connection = DriverManager.getConnection(
                 "jdbc:postgresql://localhost:5432/chatDataBase",
                 "postgres",
-                "admin"
-        )) {
+                "admin");
+             BufferedReader bis = new BufferedReader(new InputStreamReader(System.in))
+        ) {
 
+            System.out.println("Enter a message ID");
+            int inputId = Integer.parseInt(bis.readLine());
+            System.out.println("inputId=" + inputId);
             Statement statement = connection.createStatement();
-            ResultSet results = statement.executeQuery("select * from chat.user");
+            ResultSet results = statement.executeQuery("select * from chat.message where id =" + inputId);
             while (results.next()) {
 
                 int id = results.getInt(1);
-                String name = results.getString(2);
-                System.out.println(results.getRow() + ". " + id + "\t" + name);
-            }
+                int author = results.getInt(2);
+                int room = results.getInt(3);
+                String text = results.getString(4);
+                String date = results.getString(5);
 
-            int rowsCount = statement.executeUpdate("UPDATE chat.user set password='newPass' where id=1 ");
-            System.out.println("rowsCount = " +  rowsCount);
+                System.out.printf("id = %d author = %d room = %d text = %s date = %s",
+                        id,
+                        author,
+                        room,
+                        text,
+                        date);
+            }
 
 
         } catch (SQLException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
