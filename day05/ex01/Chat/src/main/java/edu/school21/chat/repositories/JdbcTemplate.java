f37288f9308418edd379d465aa0689fa4c6debe5
skiflok <anisimov.a.v@inbox.ru>
2023-05-20 12:41:18 +0300

    01 create JdbcTemplate

---
+++ day05/ex01/Chat/src/main/java/edu/school21/chat/repositories/JdbcTemplate.java
@@ -0,0 +1,70 @@
+package edu.school21.chat.repositories;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Objects;
+
+public class JdbcTemplate {
+
+    @FunctionalInterface
+    public interface SQLFunction<T, R> {
+        R apply(T object) throws SQLException;
+    }
+
+    @FunctionalInterface
+    public interface SQLConsumer<T> {
+        void accept(T object) throws SQLException;
+    }
+
+    public void connection(SQLConsumer<? super Connection> consumer) throws SQLException {
+        Objects.requireNonNull(consumer);
+        try (Connection conn = HikariCPDataSource.getConnection()) {
+            consumer.accept(conn);
+        }
+    }
+
+    public static <R> R connection(SQLFunction<? super Connection, ? extends R> function) throws SQLException {
+        Objects.requireNonNull(function);
+        try (Connection conn = HikariCPDataSource.getConnection()) {
+            return function.apply(conn);
+        }
+    }
+
+    public <R> R statement(SQLFunction<? super Statement, ? extends R> function) throws SQLException {
+        Objects.requireNonNull(function);
+        return connection(conn -> {
+            try (Statement stmt = conn.createStatement()) {
+                return function.apply(stmt);
+            }
+        });
+    }
+
+    public void statement(SQLConsumer<? super Statement> consumer) throws SQLException {
+        Objects.requireNonNull(consumer);
+        connection(conn -> {
+            try (Statement stmt = conn.createStatement()) {
+                consumer.accept(stmt);
+            }
+        });
+    }
+
+    public static  <R> R preparedStatement(String sql, SQLFunction<? super PreparedStatement, ? extends R> function) throws SQLException {
+        Objects.requireNonNull(function);
+        return connection(conn -> {
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                return function.apply(stmt);
+            }
+        });
+    }
+
+    public void preparedStatement(String sql, SQLConsumer<? super PreparedStatement> consumer) throws SQLException {
+        Objects.requireNonNull(consumer);
+        connection(conn -> {
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                consumer.accept(stmt);
+            }
+        });
+    }
+}
