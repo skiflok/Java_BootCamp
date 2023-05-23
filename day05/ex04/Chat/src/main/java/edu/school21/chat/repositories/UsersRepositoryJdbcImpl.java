bf7322acffddf6909d2ff8ec9b60184daec1ac85
skiflok <anisimov.a.v@inbox.ru>
2023-05-23 11:34:18 +0300

    04 request

--- day05/ex04/Chat/src/main/java/edu/school21/chat/repositories/UsersRepositoryJdbcImpl.java
+++ day05/ex04/Chat/src/main/java/edu/school21/chat/repositories/UsersRepositoryJdbcImpl.java
@@ -12,6 +12,28 @@ public class UsersRepositoryJdbcImpl implements UsersRepository {
     @Override
     public List<User> findAll(int page, int size) throws SQLException {
 
+
+        /*
+with user_page as (
+select * from chat.user
+	offset 4
+	limit 4),
+t as (
+select
+-- 	*
+	up.id as user_id,
+	up.login as user_login,
+	up.password as user_pass,
+	cr.id as owner_chat_id,
+	cr.name as owner_chat_name,
+	ucr.room_id as socializes_chat_id
+from user_page up
+left join chat.chat_room cr on up.id = cr.owner
+left join chat.user_chat_room ucr on up.id = ucr.user_id
+order by up.id, cr.id, ucr.room_id)
+select t.*, cr.name as socializes_chat_name from t
+left join chat.chat_room cr on t.socializes_chat_id = cr.id
+        * */
         String sql = "Select * from chat.user";
         Optional<List<User>> optionalUserList = Optional.empty();
         try {
