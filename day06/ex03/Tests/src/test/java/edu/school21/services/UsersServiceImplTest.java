e0664bf772001de021d88fd81b9d07d76aa195fd
violator <violator@student.21-school.ru>
2023-05-26 18:04:42 +0300

    03 authenticateSuccessfully

--- day06/ex03/Tests/src/test/java/edu/school21/services/UsersServiceImplTest.java
+++ day06/ex03/Tests/src/test/java/edu/school21/services/UsersServiceImplTest.java
@@ -1,8 +1,49 @@
 package edu.school21.services;
 
+import edu.school21.exceptions.AlreadyAuthenticatedException;
+import edu.school21.models.User;
+import edu.school21.repositories.UsersRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class UsersServiceImplTest {
-    // TODO: 5/26/23
+
+    private UsersRepository usersRepository;
+    UsersServiceImpl usersService;
+
+    private User user;
+
+    @BeforeEach
+    public void init () {
+        usersRepository = Mockito.mock(UsersRepository.class);
+        usersService = new UsersServiceImpl(usersRepository);
+        user = new User(1L, "USER", "PASSWORD", false);
+        Mockito.when(usersRepository.findByLogin("USER")).thenReturn(user);
+        Mockito.doNothing().when(usersRepository).update(user);
+    }
+
+    @Test
+    public void authenticateSuccessfully() throws AlreadyAuthenticatedException {
+        assertTrue(usersService.authenticate("USER", "PASSWORD"));
+        Mockito.verify(usersRepository).findByLogin("USER");
+        Mockito.verify(usersRepository).update(user);
+    }
+
+//    @Test
+//    public void authenticateBadLogin() {
+//
+//    }
+//
+//    @Test
+//    public void authenticateBadPassword() {
+//
+//    }
+
 
 }
