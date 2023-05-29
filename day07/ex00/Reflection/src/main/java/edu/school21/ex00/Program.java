7b9d4679620b5c12f7dafa370d13efeb36c9e153
violator <violator@student.21-school.ru>
2023-05-29 18:51:37 +0300

    00 try try try (((

--- day07/ex00/Reflection/src/main/java/edu/school21/ex00/Program.java
+++ day07/ex00/Reflection/src/main/java/edu/school21/ex00/Program.java
@@ -5,49 +5,69 @@ import edu.school21.ex00.models.User;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Reflection");
 
-        User user = new User();
+        try {
+            System.out.println("Reflection");
 
-        System.out.println(user);
+            Path directory = Paths.get("day07/ex00/Reflection/src/main/java/edu/school21/ex00/models").toAbsolutePath().normalize();
 
-        System.out.println("#########");
+            List<Path> filePath = getFilesFromPath(directory);
 
-        Field[] fields = user.getClass().getDeclaredFields();
+            Arrays.stream(filePath.toArray()).forEach(System.out::println);
 
-        Arrays.stream(fields).forEach(System.out::println);
+            System.out.println("Classes:");
 
-        Class<?> clazz = user.getClass();
+            Arrays.stream(filePath.toArray()).forEachOrdered((file) -> {
+                System.out.println(file.getClass().getSimpleName());
+            });
 
-        System.out.println(clazz);
+            String classDirectory = "edu.school21.ex00.models";
+//            List<Class<?>> classes = getClassesInPackage(classDirectory);
+//
+//            for (Class<?> clazz : classes) {
+//                // Получение информации о классе
+//                System.out.println("Class Name: " + clazz.getName());
+//                System.out.println("Package Name: " + clazz.getPackage().getName());
+//
+//                System.out.println("Methods:");
+//                for (Method method : clazz.getDeclaredMethods()) {
+//                    System.out.println(method.getName());
+//                }
+//
+//                System.out.println();
+//            }
 
-        Method [] methods = clazz.getDeclaredMethods();
-
-        Arrays.stream(methods).forEach(System.out::println);
-
-        Path currentDirectory = Paths.get("day07/ex00/Reflection/src/main/java/edu/school21/ex00/models").toAbsolutePath().normalize();
-
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(currentDirectory)) {
-            for (Path path : stream) {
-                if (Files.isRegularFile(path)) {
-                    System.out.printf("path - %s\n", path);
-                }
-            }
-
-        } catch (IOException e) {
+        } catch (NotDirectoryException e) {
             e.printStackTrace();
         }
 
+    }
 
+    private static List<Path> getFilesFromPath (Path directory) throws NotDirectoryException {
+        if (!Files.isDirectory(directory)) {
+            throw new NotDirectoryException("is not directory");
+        }
+        List<Path> pathList = new ArrayList<>();
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory)) {
+            for (Path path : stream) {
+                if (Files.isRegularFile(path)) {
+                    pathList.add(path);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return pathList;
     }
 
 }
