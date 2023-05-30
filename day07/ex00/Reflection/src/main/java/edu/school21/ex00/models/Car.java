d6012f4146852f96a1e078e5bdb3b151fc9c2596
violator <violator@student.21-school.ru>
2023-05-30 14:31:03 +0300

    00 car

--- day07/ex00/Reflection/src/main/java/edu/school21/ex00/models/Car.java
+++ day07/ex00/Reflection/src/main/java/edu/school21/ex00/models/Car.java
@@ -1,6 +1,33 @@
 package edu.school21.ex00.models;
 
 
-public class Car {
+import java.math.BigDecimal;
 
+public class Car {
+    private String brand;
+    private BigDecimal coast;
+    private String color;
+    private double weight;
+
+    public Car () {
+        brand = "Volga";
+        coast = new BigDecimal("99.99");
+        color = "red";
+        weight = 1000.01;
+    }
+
+    public Car(String brand, BigDecimal coast, String color, double weight) {
+        this.brand = brand;
+        this.coast = coast;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public void bip () {
+        System.out.println("bip");
+    }
+
+    private void start () {
+        System.out.println("Ehay rjavor korito yje");
+    }
 }
