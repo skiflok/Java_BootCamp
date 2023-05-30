c2e19cbb4cb895cbfe16d4882400798722bee032
violator <violator@student.21-school.ru>
2023-05-30 15:52:16 +0300

    00 ConsoleHelper logback.xml

---
+++ day07/ex00/Reflection/src/main/java/edu/school21/ex00/utils/ConsoleHelper.java
@@ -0,0 +1,47 @@
+package edu.school21.ex00.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConsoleHelper.class);
+
+    static private final BufferedReader bis =
+            new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static void printSeparatingLine() {
+        System.out.println("---------------------");
+    }
+
+    public static String readString() {
+        while (true) {
+            try {
+                String buf = bis.readLine();
+                if (buf != null)
+                    return buf;
+            } catch (IOException e) {
+                logger.warn("Произошла ошибка при попытке ввода текста.");
+            }
+        }
+    }
+
+    public static int readInt() {
+        while (true) {
+            try {
+                return Integer.parseInt(readString().trim());
+            } catch (NumberFormatException e) {
+                logger.warn("Произошла ошибка при попытке ввода текста {}.", e.toString());
+            }
+        }
+    }
+
+}
