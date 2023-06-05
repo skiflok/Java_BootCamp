6841f3de2157e170d53e6780f623c91e8877effe
violator <violator@student.21-school.ru>
2023-06-05 16:36:07 +0300

    01 form action

--- day07/ex01/Annotation/src/main/java/edu/school21/ex01/annotationProcessor/HtmlProcessor.java
+++ day07/ex01/Annotation/src/main/java/edu/school21/ex01/annotationProcessor/HtmlProcessor.java
@@ -2,6 +2,7 @@ package edu.school21.ex01.annotationProcessor;
 
 import edu.school21.ex01.annotations.HtmlForm;
 import edu.school21.ex01.annotations.HtmlInput;
+import java.io.FilterOutputStream;
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.Arrays;
@@ -16,49 +17,47 @@ import javax.lang.model.element.TypeElement;
 
 
 @SupportedAnnotationTypes(value = {"edu.school21.ex01.annotations.HtmlForm"
-        , "edu.school21.ex01.annotations.HtmlInput"
+    , "edu.school21.ex01.annotations.HtmlInput"
 })
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 //@AutoService(Processor.class)
 public class HtmlProcessor extends AbstractProcessor {
 
-    @Override
-    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        System.out.println("annotations = " + annotations.size());
+  @Override
+  public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+
+    for (Element element : roundEnv.getElementsAnnotatedWith(HtmlForm.class)) {
+
+      if (element instanceof TypeElement) {
+        TypeElement typeElement = (TypeElement) element;
+        HtmlForm htmlForm = typeElement.getAnnotation(HtmlForm.class);
+        String filName = htmlForm.fileName();
+        String action = htmlForm.action();
+        String method = htmlForm.method();
+        String formCode = String.format("<form action = \"%s\" method = \"%s\">",
+                              action,
+                              method);
+        System.out.println(formCode);
+
+      }
+    }
+    return true;
+  }
+}
+//        System.out.println("annotations = " + annotations.size());
 
 //        annotations.forEach(System.out::println);
 
-
-
-        roundEnv.getElementsAnnotatedWith(HtmlForm.class).stream()
-                .map(element -> {
-                    HtmlForm htmlForm = element.getAnnotation(HtmlForm.class);
-                    String filName = htmlForm.fileName();
-                    String action = htmlForm.action();
-                    String method = htmlForm.method();
-
-
-                    return String.format("<form action = \"%s\" method = \"%s\">",
-                            action,
-                            method);
-                })
-                .forEach(System.out::println);
-
-
-        return true;
-    }
-}
-//    System.out.println( roundEnv.getElementsAnnotatedWith(HtmlInput.class).size());
-//    System.out.println( roundEnv.getElementsAnnotatedWith(HtmlForm.class).size());
-//    annotations.forEach(annotation -> {
-//      Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(annotation);
-//      elements.stream()
-//          .flatMap(element ->
-//              Arrays.stream(element.getAnnotationsByType(HtmlInput.class))
-//          )
-//          .forEach(ann -> {
-//            System.out.println(
-//                " аннотация = " + ann.type() + " " + ann.name() + " " + ann.placeholder());
-//          });
+//          roundEnv.getElementsAnnotatedWith(HtmlForm.class).stream()
+//                  .map(element -> {
+//                      HtmlForm htmlForm = element.getAnnotation(HtmlForm.class);
+//                      String filName = htmlForm.fileName();
+//                      String action = htmlForm.action();
+//                      String method = htmlForm.method();
 //
-//    });
+//
+//                      return String.format("<form action = \"%s\" method = \"%s\">",
+//                              action,
+//                              method);
+//                  })
+//                  .forEach(System.out::println);
