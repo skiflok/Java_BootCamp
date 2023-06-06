0fdd2a61ee9e38329abb5ded456d04cbcf8b1112
skiflok <anisimov.a.v@inbox.ru>
2023-06-06 11:26:31 +0300

    02 connection

---
+++ day07/ex02/ORM/src/main/java/edu/school21/ex02/repositories/HikariCPDataSource.java
@@ -0,0 +1,31 @@
+package edu.school21.ex02.repositories;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class HikariCPDataSource {
+
+    private static final HikariConfig config = new HikariConfig();
+    private static final HikariDataSource ds;
+
+    static {
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/chatDataBase");
+        config.setUsername("postgres");
+        config.setPassword("admin");
+        config.setMaximumPoolSize(4);
+        ds = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+
+    public static DataSource getDataSource () {
+        return ds;
+    }
+
+}
