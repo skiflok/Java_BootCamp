5090c72da43038f32ac5d63709576e52a7394485
violator <violator@student.21-school.ru>
2023-06-09 14:55:20 +0300

    00 add construct

--- day08/ex00/Spring/src/main/java/edu/school21/ex00/renderer/RendererStandardImpl.java
+++ day08/ex00/Spring/src/main/java/edu/school21/ex00/renderer/RendererStandardImpl.java
@@ -2,11 +2,15 @@ package edu.school21.ex00.renderer;
 
 import edu.school21.ex00.preProcessor.PreProcessor;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor
 public class RendererStandardImpl implements Renderer {
 
-  PreProcessor preProcessor;
+  @NonNull
+  private final PreProcessor preProcessor;
 
   @Override
   public void print(String msg) {
