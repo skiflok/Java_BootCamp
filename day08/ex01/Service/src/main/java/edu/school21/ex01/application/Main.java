b607deec49f43ee01c3eb84e1f4118efef5a6cba
violator <violator@student.21-school.ru>
2023-06-13 18:27:13 +0300

    01 UsersRepositoryJdbcTemplateImpl

--- day08/ex01/Service/src/main/java/edu/school21/ex01/application/Main.java
+++ day08/ex01/Service/src/main/java/edu/school21/ex01/application/Main.java
@@ -5,6 +5,7 @@ import edu.school21.ex01.models.User;
 import edu.school21.ex01.repositories.DataBaseInitializer;
 import edu.school21.ex01.repositories.UsersRepositoryJdbcImpl;
 import edu.school21.ex01.repositories.UsersRepositoryJdbcTemplateImpl;
+import org.springframework.beans.BeansException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.sql.DataSource;
@@ -22,15 +23,12 @@ public class Main {
     dbInit.init();
 
     try {
-      UsersRepositoryJdbcTemplateImpl usersRepositoryJdbcTemplate = ctx.getBean(
-          "usersRepositoryJdbcTemplateImpl", UsersRepositoryJdbcTemplateImpl.class);
+
+      System.out.println("####### UsersRepositoryJdbcImpl #######");
       UsersRepositoryJdbcImpl usersRepositoryJdbc = ctx.getBean("usersRepositoryJdbcImpl",
           UsersRepositoryJdbcImpl.class);
 
-      System.out.println();
       System.out.println(usersRepositoryJdbc.findById(1L));
-      System.out.println(usersRepositoryJdbcTemplate.findById(1L));
-
       System.out.println("UsersRepositoryJdbcImpl list users");
       usersRepositoryJdbc.findAll().forEach(System.out::println);
 
@@ -52,6 +50,33 @@ public class Main {
       e.printStackTrace();
     }
 
+    try {
+      System.out.println("####### UsersRepositoryJdbcTemplateImpl #######");
+      UsersRepositoryJdbcTemplateImpl usersRepositoryJdbcTemplate = ctx.getBean(
+          "usersRepositoryJdbcTemplateImpl", UsersRepositoryJdbcTemplateImpl.class);
+
+      System.out.println(usersRepositoryJdbcTemplate.findById(1L));
+
+      usersRepositoryJdbcTemplate.findAll().forEach(System.out::println);
+
+      User user2 = new User(0L, "####@mail.ru");
+      usersRepositoryJdbcTemplate.save(user2);
+      System.out.println("SAVE " + usersRepositoryJdbcTemplate.findById(user2.getId()).orElse(null));
+
+      user2.setEmail("piy piy");
+      usersRepositoryJdbcTemplate.update(user2);
+      System.out.println("UPDATE " + usersRepositoryJdbcTemplate.findById(user2.getId()).orElse(null));
+
+      usersRepositoryJdbcTemplate.delete(user2.getId());
+      System.out.println("DELETE " + usersRepositoryJdbcTemplate.findById(user2.getId()).orElse(null));
+
+      System.out.println("find by email " + usersRepositoryJdbcTemplate.findByEmail("user5@example.com"));
+
+    } catch (BeansException e) {
+      e.printStackTrace();
     }
+
+
+  }
 
   }
--- day08/ex01/Service/src/main/java/edu/school21/ex01/repositories/UsersRepositoryJdbcTemplateImpl.java
+++ day08/ex01/Service/src/main/java/edu/school21/ex01/repositories/UsersRepositoryJdbcTemplateImpl.java
@@ -2,22 +2,23 @@ package edu.school21.ex01.repositories;
 
 import edu.school21.ex01.models.User;
 
+import java.util.Objects;
 import javax.sql.DataSource;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.lang.NonNull;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 
-public class UsersRepositoryJdbcTemplateImpl implements UsersRepository{
 
+public class UsersRepositoryJdbcTemplateImpl implements UsersRepository {
 
-  private final DataSource ds;
   private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
   public UsersRepositoryJdbcTemplateImpl(DataSource ds) {
-    this.ds = ds;
     namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(ds);
   }
 
@@ -37,26 +38,57 @@ public class UsersRepositoryJdbcTemplateImpl implements UsersRepository{
 
   @Override
   public List<User> findAll() {
-    return null;
+    String sql = "select * from chat.users";
+    return namedParameterJdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
   }
 
   @Override
   public void save(User entity) {
+    String sql = "insert into chat.users (email) values (:email)";
+    MapSqlParameterSource params = new MapSqlParameterSource();
+    params.addValue("email", entity.getEmail());
+
+    KeyHolder keyHolder = new GeneratedKeyHolder();
+    namedParameterJdbcTemplate.update(sql, params, keyHolder);
+    entity.setId(Objects.requireNonNull(keyHolder.getKey()).longValue());
 
   }
 
   @Override
   public void update(User entity) {
-
+    String sql = "update chat.users set email = :email where id = :id";
+    MapSqlParameterSource params = new MapSqlParameterSource();
+    params.addValue("id", entity.getId());
+    params.addValue("email", entity.getEmail());
+    if (namedParameterJdbcTemplate.update(sql, params) == 0) {
+      throw new IllegalArgumentException("Failed to update entity");
+    }
   }
 
   @Override
   public void delete(Long id) {
+    String sql = "delete from chat.users where id = :id;";
 
+    if (namedParameterJdbcTemplate.update(sql,
+        new MapSqlParameterSource().addValue("id", id)) == 0) {
+      throw new IllegalArgumentException("Failed to delete entity");
+    }
   }
 
   @Override
   public Optional<User> findByEmail(String email) {
-    return Optional.empty();
+    String sql = "select * from chat.users where email = :email";
+    MapSqlParameterSource params = new MapSqlParameterSource();
+    params.addValue("email", email);
+    List<User> users = namedParameterJdbcTemplate.query(sql, params, (rs, rowNum) ->
+        new User(rs.getLong("id"), rs.getString("email"))
+    );
+
+    if (users.isEmpty()) {
+      return Optional.empty();
+    } else {
+      return Optional.of(users.get(0));
+    }
+
   }
 }
