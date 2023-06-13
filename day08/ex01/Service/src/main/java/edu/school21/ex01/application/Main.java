b1e0ae9ad9cf9c03a9f76eb66310b1051b1e6ef3
violator <violator@student.21-school.ru>
2023-06-13 16:39:43 +0300

    01 UsersRepositoryJdbcImpl

--- day08/ex01/Service/src/main/java/edu/school21/ex01/application/Main.java
+++ day08/ex01/Service/src/main/java/edu/school21/ex01/application/Main.java
@@ -1,6 +1,7 @@
 package edu.school21.ex01.application;
 
 import edu.school21.ex01.models.Test;
+import edu.school21.ex01.models.User;
 import edu.school21.ex01.repositories.DataBaseInitializer;
 import edu.school21.ex01.repositories.UsersRepositoryJdbcImpl;
 import edu.school21.ex01.repositories.UsersRepositoryJdbcTemplateImpl;
@@ -20,17 +21,37 @@ public class Main {
     DataBaseInitializer dbInit = new DataBaseInitializer(hikariDataSource);
     dbInit.init();
 
-    UsersRepositoryJdbcTemplateImpl usersRepositoryJdbcTemplate = ctx.getBean(
-        "usersRepositoryJdbcTemplateImpl", UsersRepositoryJdbcTemplateImpl.class);
-    UsersRepositoryJdbcImpl usersRepositoryJdbc = ctx.getBean("usersRepositoryJdbcImpl",
-        UsersRepositoryJdbcImpl.class);
+    try {
+      UsersRepositoryJdbcTemplateImpl usersRepositoryJdbcTemplate = ctx.getBean(
+          "usersRepositoryJdbcTemplateImpl", UsersRepositoryJdbcTemplateImpl.class);
+      UsersRepositoryJdbcImpl usersRepositoryJdbc = ctx.getBean("usersRepositoryJdbcImpl",
+          UsersRepositoryJdbcImpl.class);
 
-    System.out.println();
-    System.out.println(usersRepositoryJdbc.findById(1L));
-    System.out.println(usersRepositoryJdbcTemplate.findById(1L));
+      System.out.println();
+      System.out.println(usersRepositoryJdbc.findById(1L));
+      System.out.println(usersRepositoryJdbcTemplate.findById(1L));
 
+      System.out.println("UsersRepositoryJdbcImpl list users");
+      usersRepositoryJdbc.findAll().forEach(System.out::println);
 
+      User testUser = new User(0, "new email");
+      usersRepositoryJdbc.save(testUser);
+      System.out.println(usersRepositoryJdbc.findById(11L).orElseThrow(RuntimeException::new));
+
+      testUser.setEmail("update email");
+      usersRepositoryJdbc.update(testUser);
+
+      System.out.println(usersRepositoryJdbc.findById(11L).orElseThrow(RuntimeException::new));
+
+      usersRepositoryJdbc.delete(11L);
+      System.out.println(usersRepositoryJdbc.findById(11L).orElse(null));
+
+      System.out.println("usersRepositoryJdbc.findByEmail " + usersRepositoryJdbc.findByEmail("user5@example.com").orElse(null));
+
+    } catch (RuntimeException e) {
+      e.printStackTrace();
+    }
+
+    }
 
   }
-
-}
--- day08/ex01/Service/src/main/java/edu/school21/ex01/repositories/UsersRepositoryJdbcImpl.java
+++ day08/ex01/Service/src/main/java/edu/school21/ex01/repositories/UsersRepositoryJdbcImpl.java
@@ -1,6 +1,8 @@
 package edu.school21.ex01.repositories;
 
 import edu.school21.ex01.models.User;
+import java.sql.Statement;
+import java.util.ArrayList;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
@@ -14,49 +16,119 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UsersRepositoryJdbcImpl implements UsersRepository {
 
-    @NonNull
-    private final DataSource ds;
+  @NonNull
+  private final DataSource ds;
 
-    @Override
-    public Optional<User> findById(Long id) {
-        String sql = "select * from chat.users where id = ?";
+  @Override
+  public Optional<User> findById(Long id) {
+    String sql = "select * from chat.users where id = ?";
 
-        try (PreparedStatement p = ds.getConnection().prepareStatement(sql)) {
-            p.setLong(1, id);
-            ResultSet resultSet = p.executeQuery();
-            if (!resultSet.next()) {
-                return Optional.empty();
-            }
-            return Optional.of(new User(resultSet.getLong(1),
-                resultSet.getString(2)));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    try (PreparedStatement p = ds.getConnection().prepareStatement(sql)) {
+      p.setLong(1, id);
+      ResultSet resultSet = p.executeQuery();
+      if (!resultSet.next()) {
         return Optional.empty();
+      }
+      return Optional.of(new User(resultSet.getLong(1),
+          resultSet.getString(2)));
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return Optional.empty();
+  }
+
+  @Override
+  public List<User> findAll() {
+    List<User> userList = new ArrayList<>();
+
+    String sql = "select * from chat.users";
+
+    try (PreparedStatement p = ds.getConnection().prepareStatement(sql)) {
+      ResultSet resultSet = p.executeQuery();
+      while (resultSet.next()) {
+        userList.add(new User(
+            resultSet.getLong("id"),
+            resultSet.getString("email")
+        ));
+      }
+
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
 
-    @Override
-    public List<User> findAll() {
-        return null;
+    return userList;
+  }
+
+  @Override
+  public void save(User entity) {
+    String sql = "insert into chat.users (email) values (?)";
+
+    try (PreparedStatement p = ds.getConnection()
+        .prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+      p.setString(1, entity.getEmail());
+
+      if (p.executeUpdate() == 0) {
+        throw new SQLException("User not save");
+      }
+
+      ResultSet resultSet = p.getGeneratedKeys();
+      if (resultSet.next()) {
+        entity.setId(resultSet.getLong("id"));
+      }
+
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
 
-    @Override
-    public void save(User entity) {
+  }
 
+  @Override
+  public void update(User entity) {
+    String sql = "update chat.users set email = ? where id = ?";
+
+    try (PreparedStatement p = ds.getConnection().prepareStatement(sql)) {
+
+      p.setString(1, entity.getEmail());
+      p.setLong(2, entity.getId());
+      if (p.executeUpdate() == 0) {
+        throw new SQLException("User not update");
+      }
+
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    @Override
-    public void update(User entity) {
+  @Override
+  public void delete(Long id) {
+    String sql = "delete from chat.users where id = ?";
 
+    try (PreparedStatement p = ds.getConnection().prepareStatement(sql)) {
+      p.setLong(1, id);
+
+      if (p.executeUpdate() == 0) {
+        throw new SQLException("User not delete");
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    @Override
-    public void delete(Long id) {
-
+  @Override
+  public Optional<User> findByEmail(String email) {
+    String sql = "select * from chat.users where email = ?";
+    try (PreparedStatement p = ds.getConnection().prepareStatement(sql)) {
+      p.setString(1, email);
+      ResultSet resultSet = p.executeQuery();
+      if (resultSet.next()) {
+        return Optional.of(new User(
+            resultSet.getLong("id"),
+            resultSet.getNString("email")
+        ));
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
-
-    @Override
-    public Optional<User> findByEmail(String email) {
-        return Optional.empty();
-    }
+    return Optional.empty();
+  }
 }
