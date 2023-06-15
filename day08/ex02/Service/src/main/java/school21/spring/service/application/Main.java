4a65687185a281ec68c0dda87337b4c1a0dc2b95
skiflok <anisimov.a.v@inbox.ru>
2023-06-15 09:43:18 +0300

    UsersServiceImpl.java inject

--- day08/ex02/Service/src/main/java/school21/spring/service/application/Main.java
+++ day08/ex02/Service/src/main/java/school21/spring/service/application/Main.java
@@ -9,74 +9,87 @@ import school21.spring.service.repositories.initializerDB.DataBaseInitializer;
 import school21.spring.service.repositories.UsersRepositoryJdbcImpl;
 import school21.spring.service.repositories.UsersRepositoryJdbcTemplateImpl;
 import org.springframework.beans.BeansException;
+import school21.spring.service.services.UsersServiceImpl;
 
 import javax.sql.DataSource;
 
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("app");
+    public static void main(String[] args) {
 
-    ApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-    DataBaseInitializer dbInit = ctx.getBean("dataBaseInitializer", DataBaseInitializer.class);
-    dbInit.init();
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        DataBaseInitializer dbInit = ctx.getBean("dataBaseInitializer", DataBaseInitializer.class);
+        dbInit.init();
 
-    try {
+        try {
 
-      System.out.println("####### UsersRepositoryJdbcImpl #######");
-      UsersRepositoryJdbcImpl usersRepositoryJdbc = ctx.getBean("usersRepositoryJdbcImpl",
-          UsersRepositoryJdbcImpl.class);
+            System.out.println("####### UsersRepositoryJdbcImpl #######");
+            UsersRepositoryJdbcImpl usersRepositoryJdbc = ctx.getBean("usersRepositoryJdbcImpl",
+                    UsersRepositoryJdbcImpl.class);
 
-      System.out.println(usersRepositoryJdbc.findById(1L));
-      System.out.println("UsersRepositoryJdbcImpl list users");
-      usersRepositoryJdbc.findAll().forEach(System.out::println);
+            System.out.println(usersRepositoryJdbc.findById(1L));
+            System.out.println("UsersRepositoryJdbcImpl list users");
+            usersRepositoryJdbc.findAll().forEach(System.out::println);
 
-      User testUser = new User(0, "new email", null);
-      usersRepositoryJdbc.save(testUser);
-      System.out.println(usersRepositoryJdbc.findById(11L).orElseThrow(RuntimeException::new));
+            User testUser = new User(0, "new email", null);
+            usersRepositoryJdbc.save(testUser);
+            System.out.println(usersRepositoryJdbc.findById(11L).orElseThrow(RuntimeException::new));
 
-      testUser.setEmail("update email");
-      usersRepositoryJdbc.update(testUser);
+            testUser.setEmail("update email");
+            usersRepositoryJdbc.update(testUser);
 
-      System.out.println(usersRepositoryJdbc.findById(11L).orElseThrow(RuntimeException::new));
+            System.out.println(usersRepositoryJdbc.findById(11L).orElseThrow(RuntimeException::new));
 
-      usersRepositoryJdbc.delete(11L);
-      System.out.println(usersRepositoryJdbc.findById(11L).orElse(null));
+            usersRepositoryJdbc.delete(11L);
+            System.out.println(usersRepositoryJdbc.findById(11L).orElse(null));
 
-      System.out.println("usersRepositoryJdbc.findByEmail " + usersRepositoryJdbc.findByEmail("user5@example.com").orElse(null));
+            System.out.println("usersRepositoryJdbc.findByEmail " + usersRepositoryJdbc.findByEmail("user5@example.com").orElse(null));
 
-    } catch (RuntimeException e) {
-      e.printStackTrace();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            System.out.println("####### UsersRepositoryJdbcTemplateImpl #######");
+            UsersRepositoryJdbcTemplateImpl usersRepositoryJdbcTemplate = ctx.getBean(
+                    "usersRepositoryJdbcTemplateImpl", UsersRepositoryJdbcTemplateImpl.class);
+
+            System.out.println(usersRepositoryJdbcTemplate.findById(1L));
+
+            usersRepositoryJdbcTemplate.findAll().forEach(System.out::println);
+
+            User user2 = new User(0L, "####@mail.ru", null);
+            usersRepositoryJdbcTemplate.save(user2);
+            System.out.println("SAVE " + usersRepositoryJdbcTemplate.findById(user2.getId()).orElse(null));
+
+            user2.setEmail("piy piy");
+            usersRepositoryJdbcTemplate.update(user2);
+            System.out.println("UPDATE " + usersRepositoryJdbcTemplate.findById(user2.getId()).orElse(null));
+
+            usersRepositoryJdbcTemplate.delete(user2.getId());
+            System.out.println("DELETE " + usersRepositoryJdbcTemplate.findById(user2.getId()).orElse(null));
+
+            System.out.println("find by email " + usersRepositoryJdbcTemplate.findByEmail("user5@example.com"));
+
+            System.out.println("find all");
+            usersRepositoryJdbcTemplate.findAll().forEach(System.out::println);
+
+        } catch (BeansException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            System.out.println("####### UserService #######");
+            UsersServiceImpl usersService = ctx.getBean(
+                    "usersServiceImpl", UsersServiceImpl.class);
+
+
+
+        } catch (BeansException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
-    try {
-      System.out.println("####### UsersRepositoryJdbcTemplateImpl #######");
-      UsersRepositoryJdbcTemplateImpl usersRepositoryJdbcTemplate = ctx.getBean(
-          "usersRepositoryJdbcTemplateImpl", UsersRepositoryJdbcTemplateImpl.class);
-
-      System.out.println(usersRepositoryJdbcTemplate.findById(1L));
-
-      usersRepositoryJdbcTemplate.findAll().forEach(System.out::println);
-
-      User user2 = new User(0L, "####@mail.ru", null);
-      usersRepositoryJdbcTemplate.save(user2);
-      System.out.println("SAVE " + usersRepositoryJdbcTemplate.findById(user2.getId()).orElse(null));
-
-      user2.setEmail("piy piy");
-      usersRepositoryJdbcTemplate.update(user2);
-      System.out.println("UPDATE " + usersRepositoryJdbcTemplate.findById(user2.getId()).orElse(null));
-
-      usersRepositoryJdbcTemplate.delete(user2.getId());
-      System.out.println("DELETE " + usersRepositoryJdbcTemplate.findById(user2.getId()).orElse(null));
-
-      System.out.println("find by email " + usersRepositoryJdbcTemplate.findByEmail("user5@example.com"));
-
-    } catch (BeansException e) {
-      e.printStackTrace();
-    }
-
-
-
-  }
-
-  }
+}
--- day08/ex02/Service/src/main/java/school21/spring/service/services/UsersServiceImpl.java
+++ day08/ex02/Service/src/main/java/school21/spring/service/services/UsersServiceImpl.java
@@ -1,9 +1,32 @@
 package school21.spring.service.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import school21.spring.service.repositories.UsersRepository;
+
+@Component
 public class UsersServiceImpl implements UsersService{
+
+  private final UsersRepository usersRepositoryI;
+
+  @Autowired
+  public UsersServiceImpl(@Qualifier("usersRepositoryJdbcTemplateImpl") UsersRepository usersRepositoryI) {
+    this.usersRepositoryI = usersRepositoryI;
+  }
 
   @Override
   public String signUp(String email) {
+
+    // TODO: 15.06.2023 проверить есть ли такой пользователь вернуть ошибку что ползователь существует
+
+    // TODO: 15.06.2023 сгенерировать пароль
+    // TODO: 15.06.2023 сохранить пользователя в БД
+    // TODO: 15.06.2023 записать в юзера ID и пароль
+    // TODO: 15.06.2023 Вернуть пароль
     return null;
   }
+
+
+
 }
