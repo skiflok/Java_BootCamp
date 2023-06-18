a680f87649b7a93d713ad078f3359a78262ff32a
skiflok <anisimov.a.v@inbox.ru>
2023-06-18 08:38:44 +0300

    init

---
+++ day09/Chat/src/main/java/edu/school21/sockets/App.java
@@ -0,0 +1,13 @@
+package edu.school21.sockets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
