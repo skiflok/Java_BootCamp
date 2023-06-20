9980c11572d41f56efff1d07aec043539b9cd75c
skiflok <anisimov.a.v@inbox.ru>
2023-06-20 09:07:49 +0300

    add conf property old sql

--- day09/ex00/Chat/src/main/java/edu/school21/sockets/app/Main.java
+++ day09/ex00/Chat/src/main/java/edu/school21/sockets/app/Main.java
@@ -1,9 +1,9 @@
-package edu.school21.sockets;
+package edu.school21.sockets.app;
 
 /**
  * Hello world!
  */
-public class App {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
---
+++ day09/ex00/Chat/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into chat.users(email, password) values
+('user1@gmail.com', 'Ys*#coyS)3'),
+('user2@gmail.com', 'wgILmT1EgT'),
+('user3@gmail.com', 'wLzY#TSs18'),
+('user4@gmail.com', 'qv#2SxrfXw'),
+('user5@gmail.com', 'LyG2@1fOMk'),
+('user6@gmail.com', 'Ys*#coyS)3'),
+('user7@gmail.com', 'wgILmT1EgT'),
+('user8@gmail.com', 'wLzY#TSs18'),
+('user9@gmail.com', 'qv#2SxrfXw'),
+('user10@gmail.com', 'LyG2@1fOMk');
