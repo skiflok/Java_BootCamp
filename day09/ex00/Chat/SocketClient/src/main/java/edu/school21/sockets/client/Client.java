f7a139e9a05a9bff96e5fcf692d30441b0824b48
violator <violator@student.21-school.ru>
2023-06-20 19:48:43 +0300

    some mistake

--- day09/ex00/Chat/SocketClient/src/main/java/edu/school21/sockets/client/Client.java
+++ day09/ex00/Chat/SocketClient/src/main/java/edu/school21/sockets/client/Client.java
@@ -2,6 +2,7 @@ package edu.school21.sockets.client;
 
 import edu.school21.sockets.models.Connection;
 import edu.school21.sockets.utils.ConsoleHelper;
+import java.io.IOException;
 import java.net.Socket;
 import lombok.Data;
 import org.slf4j.Logger;
@@ -14,19 +15,53 @@ public class Client {
   private static final Logger logger = LoggerFactory.getLogger(Client.class);
 
   private final int port;
+  private boolean isConnected;
+  private Connection connection;
 
   public void start() {
 
     try  {
 
       Connection connection = new Connection(new Socket("localhost", port));
-      ConsoleHelper.writeMessage(connection.receive());
-
+      handShake();
 
     } catch (Exception e) {
       logger.warn("Произошла ошибка при запуске или работе клиента {}", e.getMessage());
     }
 
+  }
+
+  private void handShake() throws IOException, ClassNotFoundException {
+
+    String command;
+    while (!isConnected) {
+
+      command = connection.receive();
+
+      if (!"Hello from Server!".equals(command)) {
+        continue;
+      }
+
+      command = connection.receive();
+      if (!"Enter username:".equals(command)) {
+        continue;
+      }
+      connection.send(ConsoleHelper.readString());
+
+      command = connection.receive();
+      command = connection.receive();
+      if (!"Enter password:".equals(command)) {
+        continue;
+      }
+      connection.send(ConsoleHelper.readString());
+
+      command = connection.receive();
+      if (!"Successful!".equals(command)) {
+        continue;
+      } else {
+        isConnected = true;
+      }
+    }
   }
 
 }
--- day09/ex00/Chat/SocketServer/src/main/java/edu/school21/sockets/server/ServerHandler.java
+++ day09/ex00/Chat/SocketServer/src/main/java/edu/school21/sockets/server/ServerHandler.java
@@ -1,21 +1,58 @@
 package edu.school21.sockets.server;
 
 import edu.school21.sockets.models.Connection;
+import edu.school21.sockets.models.User;
 import java.io.IOException;
 import java.net.Socket;
 import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Data
 public class ServerHandler {
 
+  private static final Logger logger = LoggerFactory.getLogger(Server.class);
+
   private final Socket socket;
+  private boolean isConnected;
+  private Connection connection;
 
   public void start() throws IOException, ClassNotFoundException {
+    connection = new Connection(socket);
+    logger.info("Подключение клиента с удаленного адреса {}", connection.getRemoteSocketAddress());
 
-    Connection connection = new Connection(socket);
-    connection.send("Hello from Server!");
-//    connection.receive();
-    System.out.println("Start");
+    User user = handShake();
+
+    //todo singUp user
+
+    connection.close();
+
+  }
+
+  private User handShake() throws IOException, ClassNotFoundException {
+    String command;
+    String userName = null;
+    String password = null;
+    while (!isConnected) {
+
+      connection.send("Hello from Server!");
+      command = connection.receive();
+      if (!"signUp".equals(command)) {
+        continue;
+      }
+
+      connection.send("Enter username:");
+      userName = connection.receive();
+      connection.send("Enter password:");
+      password = connection.receive();
+
+      isConnected = true;
+      connection.send("Successful!");
+      logger.info("пользователь {} подключился с IP {}", userName,
+          connection.getRemoteSocketAddress());
+    }
+
+    return new User(null, userName, password);
   }
 
 }
