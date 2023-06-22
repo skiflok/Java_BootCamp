83460777ff5e16703afdd1088544d63b4296e640
violator <violator@student.21-school.ru>
2023-06-22 17:28:53 +0300

    handler

--- day09/ex01/Chat/SocketServer/src/main/java/ex01/edu/school21/sockets/server/Server.java
+++ day09/ex01/Chat/SocketServer/src/main/java/ex01/edu/school21/sockets/server/Server.java
@@ -1,12 +1,10 @@
 package ex01.edu.school21.sockets.server;
 
-
 import ex01.edu.school21.sockets.config.SocketsApplicationConfig;
 import ex01.edu.school21.sockets.repositories.UsersRepository;
 import ex01.edu.school21.sockets.repositories.UsersRepositoryJdbcTemplateImpl;
 import ex01.edu.school21.sockets.repositories.utils.DataBaseInitializer;
 import ex01.edu.school21.sockets.services.UsersService;
-import ex01.edu.school21.sockets.utils.ConsoleHelper;
 import java.net.ServerSocket;
 import java.net.Socket;
 import org.slf4j.Logger;
@@ -14,23 +12,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
 public class Server {
 
   private static final Logger logger = LoggerFactory.getLogger(Server.class);
 
   private final int port;
 
-  private final UsersRepository usersRepository;
   private final UsersService usersService;
-
 
   public Server(int port) {
     this.port = port;
     ApplicationContext ctx = new AnnotationConfigApplicationContext(
         SocketsApplicationConfig.class);
     ctx.getBean("dataBaseInitializer", DataBaseInitializer.class).init();
-    usersRepository = ctx.getBean("usersRepositoryJdbcTemplateImpl",
+    UsersRepository usersRepository = ctx.getBean("usersRepositoryJdbcTemplateImpl",
         UsersRepositoryJdbcTemplateImpl.class);
     usersService = ctx.getBean("usersServiceImpl", UsersService.class);
   }
@@ -38,16 +33,14 @@ public class Server {
   public void start() {
 
     try (ServerSocket serverSocket = new ServerSocket(port)) {
-      ConsoleHelper.writeMessage("Чат сервер запущен.");
+      logger.info("Сервер запущен на порту {}", port);
 
-      Socket socket = serverSocket.accept();
+      while (true) {
+        Socket socket = serverSocket.accept();
+        ServerHandler serverHandler = new ServerHandler(socket, usersService);
+        new Thread(serverHandler).start();
+      }
 
-      new ServerHandler(socket, usersService).start();
-
-      System.out.println("All users");
-      usersRepository.findAll().forEach(System.out::println);
-
-      ConsoleHelper.writeMessage("Чат сервер остановлен.");
     } catch (Exception e) {
       logger.warn("Произошла ошибка при запуске или работе сервера {}", e.getMessage());
       e.printStackTrace();
@@ -55,3 +48,4 @@ public class Server {
 
   }
 }
+
--- day09/ex01/Chat/SocketServer/src/main/java/ex01/edu/school21/sockets/server/ServerHandler.java
+++ day09/ex01/Chat/SocketServer/src/main/java/ex01/edu/school21/sockets/server/ServerHandler.java
@@ -5,10 +5,11 @@ import ex01.edu.school21.sockets.models.User;
 import ex01.edu.school21.sockets.services.UsersService;
 import java.io.IOException;
 import java.net.Socket;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ServerHandler {
+public class ServerHandler implements Runnable{
 
   private static final Logger logger = LoggerFactory.getLogger(Server.class);
 
@@ -17,30 +18,44 @@ public class ServerHandler {
   private Connection connection;
   private final UsersService usersService;
 
-
   public ServerHandler(Socket socket, UsersService usersService) {
     this.socket = socket;
     this.usersService = usersService;
   }
 
 
-  public void start() throws IOException, ClassNotFoundException {
+  @SneakyThrows
+  @Override
+  public void run() {
     connection = new Connection(socket);
     logger.info("Подключение клиента с удаленного адреса {}", connection.getRemoteSocketAddress());
 
-    handShake();
+    while (true) {
 
-    //todo singUp user
+      printMenu();
+      handShake();
+      connection.close();
+      break;
+    }
 
-    connection.close();
+
 
   }
 
-  private User handShake() throws IOException, ClassNotFoundException {
+  private void printMenu() throws IOException {
+    connection.send("Hello from Server!");
+    connection.send("Available commands:");
+    connection.send("signUp");
+    connection.send("logIn");
+    connection.send("exit");
+  }
+
+
+  private void handShake() throws IOException, ClassNotFoundException {
     String command;
-    String userName = null;
-    String password = null;
-    User user = null;
+    String userName;
+    String password;
+    User user;
 
     while (!isConnected) {
 
@@ -71,7 +86,14 @@ public class ServerHandler {
           connection.getRemoteSocketAddress());
     }
 
-    return user;
   }
 
 }
+
+/*
+    1. Регистрация
+    2. Вход (в случае, если пользователь не обнаружен - обрывать соединение).
+    3. Отправка сообщения (каждый пользователь, подключенный к серверу, должен
+    получить данное сообщение)
+    4. Выход
+*/
