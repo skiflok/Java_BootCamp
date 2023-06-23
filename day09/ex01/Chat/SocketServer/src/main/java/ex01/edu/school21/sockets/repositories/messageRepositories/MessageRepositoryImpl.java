e9d6ef26d85d5fd4572a7f5a92a6f5325da67df3
violator <violator@student.21-school.ru>
2023-06-23 18:36:59 +0300

    massage change

--- day09/ex01/Chat/SocketServer/src/main/java/ex01/edu/school21/sockets/repositories/messageRepositories/MessageRepositoryImpl.java
+++ day09/ex01/Chat/SocketServer/src/main/java/ex01/edu/school21/sockets/repositories/messageRepositories/MessageRepositoryImpl.java
@@ -1,15 +1,43 @@
 package ex01.edu.school21.sockets.repositories.messageRepositories;
 
 import ex01.edu.school21.sockets.models.Message;
+import ex01.edu.school21.sockets.models.User;
+import ex01.edu.school21.sockets.repositories.userRepositories.UsersRepository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MessageRepositoryImpl implements MessageRepository {
+
+  private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+  private final UsersRepository usersRepository;
+
+  public MessageRepositoryImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate,
+      UsersRepository usersRepository) {
+    this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    this.usersRepository = usersRepository;
+  }
 
 
   @Override
   public Optional<Message> findById(Long id) {
-    return Optional.empty();
+    String sql = "select * from chat.message where id = :id;";
+    MapSqlParameterSource params = new MapSqlParameterSource();
+    params.addValue("id", id);
+
+    return namedParameterJdbcTemplate.query(sql, params, (rs) -> {
+      if (rs.next()) {
+        return Optional.of(new Message(
+            usersRepository.findById(rs.getLong("id")).orElseThrow(IllegalArgumentException::new),
+            rs.getString("text"),
+            rs.getString("password")
+        ));
+      }
+      return Optional.empty();
+    });
   }
 
   @Override
--- day09/ex01/Chat/Utils/src/main/java/ex01/edu/school21/sockets/models/Message.java
+++ day09/ex01/Chat/Utils/src/main/java/ex01/edu/school21/sockets/models/Message.java
@@ -1,11 +1,15 @@
 package ex01.edu.school21.sockets.models;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class Message implements Serializable {
 
   private final MessageType messageType;
   private final String message;
+  private final User user;
+  private final LocalDateTime localDateTime;
+
 
   public Message(MessageType messageType) {
     this.messageType = messageType;
@@ -15,6 +19,10 @@ public class Message implements Serializable {
   public Message(MessageType messageType, String message) {
     this.messageType = messageType;
     this.message = message;
+  }
+
+  public Message(User id, String text, String password) {
+
   }
 
   public MessageType getMessageType() {
