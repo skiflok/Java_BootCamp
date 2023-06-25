4a2ad1c1be85eb23f8f43b796151a1d902132a87
violator <violator@student.21-school.ru>
2023-06-25 17:44:43 +0300

    add room

---
+++ day09/ex02/Chat/SocketServer/src/main/java/ex02/edu/school21/sockets/models/Room.java
@@ -0,0 +1,12 @@
+package ex02.edu.school21.sockets.models;
+
+import lombok.Data;
+
+@Data
+public class Room {
+
+  private final Long id;
+  private final String name;
+  private final Long creator;
+
+}
