4e783318d70324b51dd16166f97a987ca24a95a0
violator <violator@student.21-school.ru>
2023-06-25 18:39:18 +0300

    room repository

--- day09/ex02/Chat/SocketServer/src/main/java/ex02/edu/school21/sockets/models/Room.java
+++ day09/ex02/Chat/SocketServer/src/main/java/ex02/edu/school21/sockets/models/Room.java
@@ -1,12 +1,17 @@
 package ex02.edu.school21.sockets.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Room {
 
-  private final Long id;
-  private final String name;
-  private final Long creator;
+  private Long id;
+  private String name;
+  private User creator;
 
 }
--- day09/ex02/Chat/SocketServer/src/main/java/ex02/edu/school21/sockets/repositories/roomRepositories/RoomRepositoryImpl.java
+++ day09/ex02/Chat/SocketServer/src/main/java/ex02/edu/school21/sockets/repositories/roomRepositories/RoomRepositoryImpl.java
@@ -1,33 +1,89 @@
 package ex02.edu.school21.sockets.repositories.roomRepositories;
 
 import ex02.edu.school21.sockets.models.Room;
+import ex02.edu.school21.sockets.repositories.userRepositories.UsersRepository;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
 
-public class RoomRepositoryImpl implements RoomRepository{
+@Component
+public class RoomRepositoryImpl implements RoomRepository {
+
+  private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+  private final UsersRepository usersRepository;
+
+  @Autowired
+  public RoomRepositoryImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate,
+      UsersRepository usersRepository) {
+    this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    this.usersRepository = usersRepository;
+  }
 
   @Override
   public Optional<Room> findById(Long id) {
-    return Optional.empty();
+    String sql = "select * from chat.chat_rooms where id = :id;";
+    MapSqlParameterSource params = new MapSqlParameterSource();
+    params.addValue("id", id);
+
+    return namedParameterJdbcTemplate.query(sql, params, (rs) -> {
+      if (rs.next()) {
+        return Optional.of(new Room(
+            rs.getLong("id"),
+            rs.getString("name"),
+            null
+        ));
+      }
+      return Optional.empty();
+    });
   }
 
   @Override
   public List<Room> findAll() {
-    return null;
+    return namedParameterJdbcTemplate.query("select * from chat_rooms order by id",
+        (rs, rowNum) -> {
+          return new Room(rs.getLong("id"), rs.getString("name"), null);
+        });
   }
 
   @Override
   public void save(Room entity) {
+    String sql = "insert into chat.chat_rooms (name, creator) values (:name, :creator)";
+    MapSqlParameterSource params = new MapSqlParameterSource();
+    params
+        .addValue("name", entity.getName())
+        .addValue("creator", entity.getCreator().getId());
 
+    KeyHolder keyHolder = new GeneratedKeyHolder();
+    namedParameterJdbcTemplate.update(sql, params, keyHolder);
+    entity.setId((Long) Objects.requireNonNull(keyHolder.getKeys()).get("id"));
   }
 
   @Override
   public void update(Room entity) {
-
+    String sql = "update chat.chat_rooms set name = :name, creator = :creator where id = :id";
+    MapSqlParameterSource params = new MapSqlParameterSource();
+    params
+        .addValue("id", entity.getId())
+        .addValue("name", entity.getName())
+        .addValue("creator", entity.getCreator().getId());
+    if (namedParameterJdbcTemplate.update(sql, params) == 0) {
+      throw new IllegalArgumentException("Failed to update entity");
+    }
   }
 
   @Override
   public void delete(Long id) {
+    String sql = "delete from chat.chat_rooms where id = :id;";
 
+    if (namedParameterJdbcTemplate.update(sql,
+        new MapSqlParameterSource().addValue("id", id)) == 0) {
+      throw new IllegalArgumentException("Failed to delete entity");
+    }
   }
 }
